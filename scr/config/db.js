64d6fab21211e876cbe3a376d1154a87bb785585
Biplab Mahata <biplabmahatajk@gmail.com>
2026-03-26 01:12:45 +0530

    change listing

--- scr/config/db.js
+++ scr/config/db.js
@@ -2,11 +2,24 @@ import mongoose from "mongoose";
 
 
 
+// export const connectDB = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URL);
+//         console.log("MongoDB connected successfully");
+//     } catch (error) {
+//         console.log("Error from connect to database", error);
+//     }
+// };
+
+
 export const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log("MongoDB connected successfully");
-    } catch (error) {
-        console.log("Error from connect to database", error);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB connected");
+
+  } catch (error) {
+    console.log("DB ERROR:", error.message);
+    process.exit(1);
+  }
 };
