3bdcafb66fc56a4656b74fa10e75356749eef6f6
Biplab Mahata <biplabmahatajk@gmail.com>
2026-03-25 17:29:36 +0530

    Initial commit

--- scr/models/user.model.js
+++ scr/models/user.model.js
@@ -35,12 +35,20 @@ const Userschema = new mongoose.Schema(
   { timestamps: true }
 );
 
-Userschema.pre("save", async function () {
-  if (!this.isModified("password")) return ;
+// Userschema.pre("save", async function () {
+//   if (!this.isModified("password")) return ;
+//   const salt = await bcrypt.genSalt(10);
+//   this.password = await bcrypt.hash(this.password, salt);
+// });
+
+Userschema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();  // ✅ FIX
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-});
 
+  next();  // ✅ FIX
+});
 Userschema.methods.comparePassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
