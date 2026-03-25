aee493e0ef1d2ea5779afb6ce6d51207965d2eaa
Biplab Mahata <biplabmahatajk@gmail.com>
2026-03-25 15:27:29 +0530

    Initial commit

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import { connectDB } from "./scr/config/db.js";
 import userRouter from "./scr/routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import contestRouter from "./scr/routes/contest.route.js";
+import cors from "cors";
 
 
 
@@ -14,6 +15,11 @@ app.use(cookieParser());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 
 app.use("/api/v1/auth", userRouter);
 app.use("/api/v1/contest", contestRouter);
