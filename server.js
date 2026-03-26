f0f82c88f589b89d71f6097c565f50d4b152d9e7
Biplab Mahata <biplabmahatajk@gmail.com>
2026-03-26 14:13:07 +0530

    change origin

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(cors({
   origin: [
-    "https://desunlearnanderan.netlify.app/"
+    "https://desunlearnanderan.netlify.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
