8d851d5af014d816c565fc97405b56efbd688f51
Biplab Mahata <biplabmahatajk@gmail.com>
2026-04-15 00:03:20 +0530

    add category

--- scr/models/contest.model.js
+++ scr/models/contest.model.js
@@ -1,51 +1,3 @@
-// import mongoose from "mongoose";
-
-// const contestSchema = new mongoose.Schema(
-
-//   {
-//     title: {
-//       type: String,
-//       required: true
-//     },
-
-//     description: String,
-
-//     image: {
-//       type: String
-//     },
-
-//     startDate: {
-//       type: Date,
-//       required: true 
-//     },
-
-//     deadline: {
-//       type: Date,
-//       required: true
-//     },
-
-//     rewards: [{
-//       type: String
-//     }],
-
-//     createdBy: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "User",
-//       required: true
-//     },
-
-//     status: {
-//       type: String,
-//       enum: ["upcoming", "active", "completed"],
-//       default: "upcoming"  
-//     }
-//   },
-//   { timestamps: true }
-// );
-
-// export const Contest = mongoose.model("Contest", contestSchema);
-
-// console.log("contest model is working");
 
 import mongoose from "mongoose";
 
@@ -165,7 +117,7 @@ contestSchema.path("maxTeamSize").validate(function (value) {
   }
 
   return value === 1;
-}, "Team or both-mode contests must allow at least 2 members, and solo contests must have a maxTeamSize of 1.");
+}, "Team contests must allow at least 2 members, and solo contests must have a maxTeamSize of 1.");
 
 contestSchema.methods.syncStatus = function () {
   this.status = getContestStatus(this);
