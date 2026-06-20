b4219432733fb3086ccbe4bfe9e56ddd89d7b5d1
Biplab Mahata <137335325+Biplab29@users.noreply.github.com>
2026-06-21 01:07:18 +0530

    Add new origin to CORS configuration

--- scr/app.js
+++ scr/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://learnearnweb.netlify.app"],
+    origin: ["http://localhost:5173", "https://learnearnweb.netlify.app", "https://learn-earn-contest.netlify.app"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
